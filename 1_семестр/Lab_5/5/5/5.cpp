3d9db7ef4abcf2702f8ae8dce35a266febbc8b69
Eugene <cullofdute19762005@gmail.com>
2024-02-16 01:24:37 +0300

    Revert "дописал программу, не выполняет основную функцию"
    This reverts commit 09d8038b0467ca81bebaf59bed30ce9c89a2b5e3.

--- 1_семестр/Lab_5/5/5/5.cpp
+++ 1_семестр/Lab_5/5/5/5.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <vector>
 #include <map>
 #include <string>
--- 2_семестр/L_1/1.4/1.4/1.4.cpp
+++ 2_семестр/L_1/1.4/1.4/1.4.cpp
@@ -23,40 +23,10 @@ int main()
 		std::cout << "Failed to open '" << inputFileName << "' for reading\n";
 		return 1;
 	}
-	
-	int lines, columns;
 
-	input >> lines >> columns;
+	int Lines, Columns;
+	char ch;
 
-	std::vector<std::vector<int>> field(lines, std::vector<int>(columns, 0));
-
-	for (int i = 0; i < lines; i++)
-	{
-		for (int j = 0; j < columns; j++)
-		{
-			char ch;
-			input >> ch;
-			field[i][j] = ch - '0';
-			std::cout << field[i][j];
-		}
-		std::cout << '\n';
-	}
-
-	auto val = 0;
-	for (int i = 0; i < lines; i++)
-	{
-		for (int j = 0; j < columns; j++)
-		{
-			if (field[i][j] == 1) {
-				if (i == 0 || field[i - 1][j] == 0) val++;
-				if (i == lines - 1 || field[i + 1][j] == 0) val++;
-				if (j == 0 || field[i][j - 1] == 0) val++;
-				if (j == columns - 1 || field[i][j + 1]) val++;
-			}
-		}
-	}
-
-	std::cout << val << '\n';
 
     return 0;
 }
